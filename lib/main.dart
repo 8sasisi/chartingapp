9a88bb4c735d7ed7ba08c5ac917b430a246dc7f6
samm <sammsasisi@gmail.com>
2026-02-13 10:17:25 -0800

    Update Flutter project: main.dart, Firebase, pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,9 @@ import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-Future <void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-  )
+  );
 
   runApp(const MyApp());
 }
